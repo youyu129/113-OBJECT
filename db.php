4b563d1fca98cc9fe8c59bca3282f8676d8497af
youyu <129youyu@gmail.com>
2024-11-26 14:11:50 +0800

    新增在類別內建立取出單筆和多筆的兩個共用函式

--- db.php
+++ db.php
@@ -8,20 +8,51 @@ class DB {
     protected $table;
 
     function __construct($table){
+        // this是指上面的DB
         $this->table=$table;
         // 資料庫沒有設密碼
         // pdo的目的：把資料傳出去
         $this->pdo=new PDO($this->dsn,'root'.'');
     }
 
+    /*
+    * 撈出全部資料
+    *
+    */
+    // 下面隨時可以呼叫$this->table $this->pdo
+    // 下面的function可以隨時取用這兩個變數 $pdo $table
     function all(){
         // $table就是dept
         return $this->q("SELECT * FROM $this->table");
     }
-    function q($sql){
-        return $this->pdo->query($sql)->fetchAll();
+
+    /*
+    * 把陣列轉成條件字串陣列
+    */
+    // array to string
+    function a2s($array){
+        $tmp=[];
+        foreach($array as $key => $value){
+            $tmp[]="`$key`='$value'";
+        }
+        return $tmp;
     }
+    function fetchOne($sql){
+        // debug用：
+        //echo $sql;
+        return $this->$pdo->query($sql)->fetch();
+    }
+    function fetchAll($sql){
+        // debug用：
+        //echo $sql;
+        return $this->$pdo->query($sql)->fetchAll();
+    }
+    
 }
+
+// function q($sql){
+//     return $this->pdo->query($sql)->fetchAll();
+// }
 
 // 列印陣列的function
 function dd($array){
@@ -30,6 +61,7 @@ function dd($array){
     echo "</pre>";
 }
 
+// 實例化
 $DEPT=new DB('dept');
 
 // 原本$dept=$DEPT->q("SELECT * FROM dept");
